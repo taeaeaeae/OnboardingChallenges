edcebdce41f8534362753facf723399142b64123
taeaeaeae <haenimhana@gmail.com>
2024-12-07 17:52:55 +0900

    init

---
+++ src/test/java/com/taekyoung/onboardingchallenges/OnboardingChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taekyoung.onboardingchallenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingChallengesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
